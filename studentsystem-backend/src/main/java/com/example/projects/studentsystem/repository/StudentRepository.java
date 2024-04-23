493e83b72222294d300900563dc007eb3c4e1ccf
Sejal Jadhav <sejaljadhaav0508@gmail.com>
2024-04-23 16:31:27 +0530

    frontend changes

--- studentsystem-backend/src/main/java/com/example/projects/studentsystem/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.projects.studentsystem.repository;
-
-import com.example.projects.studentsystem.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-}
